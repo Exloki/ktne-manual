a433b5f81342d0c4c25b93731eb79960da03ab9b
James H <Exlokis@live.co.uk>
2025-04-12 21:48:05 +0100

    oops, forgot to commit the actual module

---
+++ src/components/EasyModeToggle.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './EasyModeToggle.css';
+
+// Create a context to share the easy mode state across components
+export const EasyModeContext = React.createContext<boolean>(false);
+
+interface EasyModeToggleProps {
+  isEasyMode: boolean;
+  toggleEasyMode: () => void;
+}
+
+const EasyModeToggle: React.FC<EasyModeToggleProps> = ({ isEasyMode, toggleEasyMode }) => {
+  return (
+    <div className="easy-mode-toggle">
+      <div className="easy-mode-label">Easy Mode</div>
+      <label className="toggle-switch">
+        <input
+          type="checkbox"
+          checked={isEasyMode}
+          onChange={toggleEasyMode}
+        />
+        <span className="toggle-slider"></span>
+      </label>
+      <div className="tooltip">Easy mode will enable near-automatic solving for Simple Wires and Morse Code</div>
+    </div>
+  );
+};
+
+export default EasyModeToggle; 
